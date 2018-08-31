c52858201fe829d922b7e35303416f90b001c212
Jonathan Reeves <jonathanedwardreeves1988@gmail.com>
2018-08-30 19:52:18 -0500

    Updating views

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,35 @@
 /* Get 'home' page */
 module.exports.homelist = function(req, res) {
-  res.render('locations-list', { title: 'Home' });
+  res.render('locations-list', {
+    title: 'Loc8r - find a place to work with wifi',
+    pageHeader: {
+      title: 'Loc8r',
+      strapline: 'Find places to work with wifi near you!',
+    },
+    locations: [
+      {
+        name: 'Starcups',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium Wi-Fi'],
+        distance: '100m',
+      },
+      {
+        name: 'Cafe Hero',
+        address: '127 Fairy Ln, Imaginationland, USA',
+        rating: 4,
+        facilities: ['Hot drinks', 'Food', 'Premium Wi-Fi'],
+        distance: '200m',
+      },
+      {
+        name: 'Burger Queen',
+        address: ' 2343 Heart Drive, Wonderland, USA',
+        rating: 5,
+        facilities: ['Hot drinks', 'Premium Wi-Fi'],
+        distance: '350m',
+      },
+    ],
+  });
 };
 
 /* Get 'Location info' page */
